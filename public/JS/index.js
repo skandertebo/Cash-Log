5e000a8bca640cf9bb1be0d5dd73df9fcfcbb831
skandertebo <alexandertebourb@gmail.com>
2022-07-09 03:32:15 +0100

    updating layout

--- public/JS/index.js
+++ public/JS/index.js
@@ -1,16 +1,18 @@
 let addCategoryBtn = document.getElementById("add-cat-btn");
 addCategoryBtn.addEventListener("click", ()=>{
-    let newCategory = document.createElement('div');
-    newCategory.setAttribute("class" , "form-check mb-2");
-    newCategory.setAttribute("style" , "display:flex; align-items:center; justify-content:flex-start;");
-    newCategory.innerHTML = '<input type="radio" name="categorySelect" class="form-check-input" value=' + document.getElementById("add-cat").value + '> <label for="categorySelect" class="form-check-label mx-2">  ' + document.getElementById("add-cat").value + ' </label>';
-    document.querySelector(".category-list").appendChild(newCategory);
-    let newCategoryInOverview = document.createElement('li');
-    newCategoryInOverview.setAttribute("class","list-group-item");
-    newCategoryInOverview.setAttribute("style","background:none;");
-    newCategoryInOverview.textContent = document.getElementById("add-cat").value; +" : 0TND"
-    document.getElementById('category-list-overview').appendChild(newCategoryInOverview);
-    document.getElementById('new-category').value = document.getElementById("add-cat").value ;
-    document.getElementById('addCategory').submit();
-    document.getElementById("add-cat").value = '';
+    if(document.getElementById("add-cat").value!=''){
+        let newCategory = document.createElement('div');
+        newCategory.setAttribute("class" , "form-check mb-2");
+        newCategory.setAttribute("style" , "display:flex; align-items:center; justify-content:flex-start;");
+        newCategory.innerHTML = '<input type="radio" name="categorySelect" class="form-check-input" value=' + document.getElementById("add-cat").value + '> <label for="categorySelect" class="form-check-label mx-2">  ' + document.getElementById("add-cat").value + ' </label>';
+        document.querySelector(".category-list").appendChild(newCategory);
+        let newCategoryInOverview = document.createElement('li');
+        newCategoryInOverview.setAttribute("class","list-group-item");
+        newCategoryInOverview.setAttribute("style","background:none;");
+        newCategoryInOverview.textContent = document.getElementById("add-cat").value; +" : 0TND"
+        document.getElementById('category-list-overview').appendChild(newCategoryInOverview);
+        document.getElementById('new-category').value = document.getElementById("add-cat").value ;
+        document.getElementById('addCategory').submit();
+        document.getElementById("add-cat").value = '';
+    }
 })
